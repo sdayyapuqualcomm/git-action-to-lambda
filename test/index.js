502d73a4014341b528e56c40669816c509f09683
sdayyapuqualcomm <98131617+sdayyapuqualcomm@users.noreply.github.com>
2022-03-16 13:38:46 -0700

    Update index.js

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda - CICD!'),
+        body: JSON.stringify('Hello from Lambda Test - CICD!'),
     };
     return response;
 };
